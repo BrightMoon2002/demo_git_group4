df69eb062cb6f3ffa9d07f947d41f0a257ae19ea
hieuhvh1994 <hieudaohn94@gmail.com>
2021-11-15 15:09:50 +0700

    test3

--- src/main/java/model/student.java
+++ src/main/java/model/student.java
@@ -3,29 +3,17 @@ package model;
 public class student {
     private int id;
     private String name;
+    private String lop;
 
     public student(String name) {
         this.name = name;
     }
 
-    public student(int id, String name) {
+    public student(int id, String name, String lop) {
         this.id = id;
         this.name = name;
+        this.lop = lop;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    
 }
